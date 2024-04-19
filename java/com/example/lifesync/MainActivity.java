0d26ea2de2837c825a5875731cfd778ad511c28a
Rafay Chohan <abrkchohan@gmail.com>
2024-04-20 00:48:23 +0500

    tested multi page

--- java/com/example/lifesync/MainActivity.java
+++ java/com/example/lifesync/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.lifesync;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn =(Button)findViewById(R.id.btn1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(MainActivity.this,Page2.class);
+                startActivity(android.content.intent);
+            }
+        });
     }
 }
---
+++ java/com/example/lifesync/Page2.java
@@ -0,0 +1,17 @@
+package com.example.lifesync;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Page2 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_page);
+
+    }
+}
+
